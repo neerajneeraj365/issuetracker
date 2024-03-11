dba478e39e1163b6b45854a143ad4bf2ff926004
Neeraj Neeraj <neerajneeraj@Neerajs-MacBook-Air.local>
2024-03-10 22:25:21 -0400

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -7,38 +7,40 @@ import { AiFillBug } from "react-icons/ai";
 
 export default function Home() {
   const [data, setData] = useState([]);
-  console.log("this is the state data", data);
 
   useEffect(() => {
     getIssue();
-  }, [0]);
+  }, []);
 
   const getIssue = async () => {
     try {
-      const response = await axios.get("/api/issues");
-      const serverData = response.data;
-      setData(serverData);
+      const { data: response } = await axios.get("/api/issues");
+      setData(response);
+      console.log(response);
     } catch (error) {
       console.error("Can not get data", error);
     }
   };
+
   return (
-    <div className="grid grid-rows-1 md:grid-rows-2 grid-flow-row md:grid-flow-col gap-4">
-      {data.map((i) => (
-        <Card style={{ maxWidth: 300 }} key={i.id} size="3" className="m-5">
-          <Box className="">
-            <AiFillBug />
-            <Text as="p" size="2" weight="bold" color="red">
-              {i.title}
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      {data.map(({ id, title, description, createdAt, status }) => (
+        <Card key={id} className="">
+          <Box>
+            <div className="flex items-center text-red-400">
+              <AiFillBug />
+              <Text as="div" size="2" weight="bold">
+                {title}
+              </Text>
+            </div>
+            <Text as="div" size="2">
+              {description}
             </Text>
-            <Text as="p" size="2" color="red">
-              {i.description}
+            <Text as="div" size="1">
+              {createdAt}
             </Text>
-            <Text as="p" size="2" color="red">
-              {i.status}
-            </Text>
-            <Text as="p" size="2" color="red">
-              {i.createdAt}
+            <Text as="div" size="1">
+              {status}
             </Text>
           </Box>
         </Card>
